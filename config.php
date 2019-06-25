e927ddab508dc059d031f394a327873cdfa84b02
root <root@ip-172-31-14-167.ap-south-1.compute.internal>
2019-06-25 03:42:54 +0000

    this is the commit by develper B

--- config.php
+++ config.php
@@ -1,7 +1,9 @@
 
-
 <?php
 mysql_connect("localhost","root","");
 mysql_select_db("shop");
 ?>
 #this is 1500 line of source code written by develperA
+
+
+# this is the 200 lines of code by deveper B
